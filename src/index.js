29e232717ab5c34c82a0bc8312f9f4d0d8ed520c
Luis Arana <luarana631@gmail.com>
2021-04-18 16:39:50 -0600

    React Project Start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Components/Profile/Profile';
+import './index.css';
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render( <Profile /> , divRoot);
+
