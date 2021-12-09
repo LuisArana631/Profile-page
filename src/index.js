b9f2cf8240da3268d648055751576995f362b930
Luis <luarana631@gmail.com>
2021-12-09 09:35:38 -0600

    Reiniciar proyecto React

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Profile from './Components/Profile/Profile';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const divRoot = document.querySelector('#root');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render( <Profile /> , divRoot);
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
