707cb6101dfc9e6b031615e99f44c0e868d7a8d9
Bloc De Notas <luarana631@gmail.com>
2024-03-20 23:40:27 -0600

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Profile-page',
+  assetPrefix: '/Profile-page/',
+  reactStrictMode: true,
 };
 
 export default nextConfig;
