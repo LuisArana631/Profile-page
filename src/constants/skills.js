a99c61fe250d026dccf12a0ea2a50245ec269a4e
Bloc De Notas <luarana631@gmail.com>
2024-03-21 00:21:24 -0600

    last try

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -2,62 +2,62 @@ const SKILLS = [
   {
     id: 'nodejs',
     name: 'Node JS',
-    icon: '/node_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/node_icon.svg',
   },
   {
     id: 'javascript',
     name: 'Javascript',
-    icon: '/js_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/js_icon.svg',
   },
   {
     id: 'react',
     name: 'React',
-    icon: '/react_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/react_icon.svg',
   },
   {
     id: 'html',
     name: 'HTML',
-    icon: '/html_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/html_icon.svg',
   },
   {
     id: 'css',
     name: 'CSS',
-    icon: '/css_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/css_icon.svg',
   },
   {
     id: 'git',
     name: 'Git',
-    icon: '/git_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/git_icon.svg',
   },
   {
     id: 'aws',
     name: 'AWS',
-    icon: '/aws_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/aws_icon.svg',
   },
   {
     id: 'serverless',
     name: 'Serverless',
-    icon: '/sls_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/sls_icon.svg',
   },
   {
     id: 'terminal',
     name: 'Terminal',
-    icon: '/terminal_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/terminal_icon.svg',
   },
   {
     id: 'mysql',
     name: 'Mysql',
-    icon: '/mysql_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/mysql_icon.svg',
   },
   {
     id: 'markdown',
     name: 'Markdown',
-    icon: '/md_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/md_icon.svg',
   },
   {
     id: 'vscode',
     name: 'Visual Studio Code',
-    icon: '/vscode_icon.svg',
+    icon: 'https://luisarana631.github.io/Profile-page/vscode_icon.svg',
   }
 ]
 
